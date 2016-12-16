9553c83a120f4f609fb8a8c64e3b222c815e99b9
w.wei <w.wei@Ctrip.com>
2016-12-16 17:37:35 +0800

    adjust package name and file location.

--- ReferenceManagement/src/main/java/com/ctrip/smarttrip/tools/refmgr/controller/SoaController.java
+++ ReferenceManagement/src/main/java/com/ctrip/smarttrip/tools/refmgr/controller/SoaController.java
@@ -1,12 +1,12 @@
-package controller;
+package com.ctrip.smarttrip.tools.refmgr.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SoaController {
-    @RequestMapping(value = {"/", "soa/list"})
+    @RequestMapping(value = {"/", "/soa/list"})
     public String list(){
-        return "this is list from spring mvc.";
+        return "index";
     }
 }
